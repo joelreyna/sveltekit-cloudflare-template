b8771b1321bfa1fa75cfed5226c4f59255f91d15
Joel Reyna <joelreyna1104@gmail.com>
2025-02-12 16:30:24 -0600

    fix: Pass env to requests instead of creating additional devServer

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import devServer from '@hono/vite-dev-server';
-import adapter from '@hono/vite-dev-server/cloudflare'
-import build from '@hono/vite-cloudflare-pages'
 
 export default defineConfig({
-	plugins: [
-		build(),
-		devServer({
-			adapter,
-			entry: 'src/lib/api/api.ts'
-		}),
-		sveltekit(),
-		tailwindcss(),
-	]
+	plugins: [sveltekit(), tailwindcss()]
 });
