8c3733e7d19823f0abf82a2a882f5873c5a777c4
Joel Reyna <joelreyna1104@gmail.com>
2025-02-19 13:27:38 -0600

    test

--- src/lib/api/middlewares/inject-db.ts
+++ src/lib/api/middlewares/inject-db.ts
@@ -1,14 +1,18 @@
 import { createMiddleware } from 'hono/factory';
 
 import { dbTesting } from '$lib/api/lib/test-setup';
-import { db } from '$lib/db/db';
+import { db, getProdDB } from '$lib/db/db';
+import { drizzle } from 'drizzle-orm/d1';
+import * as schema from '$lib/db/schema';
 
 export const injectDB = createMiddleware(async (c, next) => {
     if (process.env.VITEST) {
         c.set('db', dbTesting);
     } else {
+        const db = drizzle(getProdDB() as D1Database, {
+            schema
+        });
         console.log("Injecting DB")
-        console.log(JSON.stringify(db, null, 2))
         c.set('db', db);
     }
 
