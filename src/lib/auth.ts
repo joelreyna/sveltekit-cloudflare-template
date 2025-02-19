276e5c2d3ec1c8741b82cd02ffebba16e0f53e47
Joel Reyna <joelreyna1104@gmail.com>
2025-02-19 17:30:42 -0600

    test fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,28 +6,19 @@ import { db, getProdDB } from "$lib/db/db";
 import * as schema from "$lib/db/schema";
 import { drizzle } from 'drizzle-orm/d1';
 
-async function getDB() {
+function getDB() {
     if (ENVIRONMENT === "development") {
         return db as DB;
     }
-    
-    const waitForDB = () => {
-        return new Promise<DB>((resolve) => {
-            console.log("Waiting for DB");
-            console.log(getProdDB());
-            if (getProdDB() !== null) {
-                resolve(drizzle(getProdDB() as D1Database, { schema }) as DB);
-            } else {
-                setTimeout(() => waitForDB().then(resolve), 500);
-            }
-        });
-    };
-
-    return await waitForDB();
+    console.log("Waiting for DB");
+    console.log(getProdDB());
+    return drizzle(getProdDB() as D1Database, {
+        schema
+    }) as DB;
 }
 
 export const auth = betterAuth({
-    database: drizzleAdapter(await getDB(), {
+    database: drizzleAdapter(getDB(), {
         provider: "sqlite",
         schema: schema
     }),
