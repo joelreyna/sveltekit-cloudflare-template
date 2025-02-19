ec68ee26e15120065fd6d878606b12fbd8173e4c
Joel Reyna <joelreyna1104@gmail.com>
2025-02-19 14:24:37 -0600

    fix: Change DB setup in Better Auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,22 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter, type DB } from "better-auth/adapters/drizzle";
+import { ENVIRONMENT } from "$env/static/private";
 
-import { db } from "$lib/db/db";
+import { db, getProdDB } from "$lib/db/db";
 import * as schema from "$lib/db/schema";
+import { drizzle } from 'drizzle-orm/d1';
+
+function getDB() {
+    if (ENVIRONMENT === "development") {
+        return db as DB;
+    }
+    return drizzle(getProdDB() as D1Database, {
+        schema
+    }) as DB;
+}
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db as DB, {
+    database: drizzleAdapter(getDB(), {
         provider: "sqlite",
         schema: schema
     }),
