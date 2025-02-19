0c854cf06102e5a7892cc68e1c21b11d9c054678
Joel Reyna <joelreyna1104@gmail.com>
2025-02-19 13:18:15 -0600

    test envs

--- src/lib/api/middlewares/set-prod-db.ts
+++ src/lib/api/middlewares/set-prod-db.ts
@@ -3,7 +3,12 @@ import { getProdDB, setProdDB } from '$lib/db/db';
 import { ENVIRONMENT } from '$env/static/private';
 
 export const setProdDBMiddleware = createMiddleware(async (c, next) => {
+    console.log("Setting Prod DB")
+    console.log(ENVIRONMENT)
+    console.log(getProdDB())
+    console.log(c.env.DB)
     if (ENVIRONMENT === "production" && getProdDB() === null) {
+        console.log("Entered Setting Prod DB")
         setProdDB(c.env.DB);
     }
     return await next();
