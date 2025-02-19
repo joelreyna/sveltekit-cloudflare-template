d237e7fb0c200659d2d06f7f6a169f22e794a485
Joel Reyna <joelreyna1104@gmail.com>
2025-02-19 12:22:53 -0600

    fix: test envs

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,20 +2,23 @@ import { getPlatformProxy } from "wrangler";
 import { drizzle } from 'drizzle-orm/d1';
 
 import * as schema from '$lib/db/schema';
-const { env } = await getPlatformProxy();
+
 
 async function initDbConnectionDev() {
-    return drizzle(env.DB as D1Database, {
-        schema
-    });
+    if (process.env.NODE_ENV === "development") {
+        const { env } = await getPlatformProxy();
+        return drizzle(env.DB as D1Database, {
+            schema
+        });
+    }
 }
 
-async function initDbConnection() {
+function initDbConnection() {
     console.log(process.env)
-    console.log(env)
-    return drizzle(env.DB as D1Database, {
+
+    return drizzle(process.env.DB as unknown as D1Database, {
         schema
     });
 }
 
-export const db = process.env.NODE_ENV === "production" ? await initDbConnection() : await initDbConnectionDev()
+export const db = process.env.NODE_ENV === "production" ? initDbConnection() : await initDbConnectionDev()
