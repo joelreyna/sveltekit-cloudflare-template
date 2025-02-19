25f63009e0dce2c31e06cfb279902e35ee9a122b
Joel Reyna <joelreyna1104@gmail.com>
2025-02-19 17:46:38 -0600

    test

--- src/lib/api/middlewares/set-prod-db.ts
+++ src/lib/api/middlewares/set-prod-db.ts
@@ -1,9 +1,11 @@
 import { createMiddleware } from 'hono/factory';
 import { getProdDB, setProdDB } from '$lib/db/db';
 import { ENVIRONMENT } from '$env/static/private';
+import * as process from 'node:process';
 
 export const setProdDBMiddleware = createMiddleware(async (c, next) => {
     if (ENVIRONMENT === "production" && getProdDB() === null) {
+        process.env.DB = c.env.DB;
         setProdDB(c.env.DB);
     }
     return await next();
