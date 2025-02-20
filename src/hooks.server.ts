07ae40c14232b5a0dfbf9170ff6ce82e788d11bd
Joel Reyna <joelreyna1104@gmail.com>
2025-02-19 21:40:39 -0600

    test auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
-import { auth } from "$lib/auth";
+import { initializeAuth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 
 export async function handle({ event, resolve }) {
+    const auth = initializeAuth(event.platform?.env.DB);
     return svelteKitHandler({ event, resolve, auth });
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,32 +2,28 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter, type DB } from "better-auth/adapters/drizzle";
 import { ENVIRONMENT } from "$env/static/private";
 
-import { db, getProdDB } from "$lib/db/db";
+import { db } from "$lib/db/db";
 import * as schema from "$lib/db/schema";
 import { drizzle } from 'drizzle-orm/d1';
 
-import * as process from 'node:process';
+export const initializeAuth = (d1Database?: D1Database) => {
+    let _db;
 
-function getDB() {
     if (ENVIRONMENT === "development") {
-        return db as DB;
+        _db = db as DB;
+    } else {
+        _db = drizzle(d1Database as D1Database, {
+            schema
+        }) as DB;
     }
 
-    console.log("Waiting for DB");
-    console.log(process);
-    console.log(JSON.stringify(process.env));
-
-    return drizzle(getProdDB() as D1Database, {
-        schema
-    }) as DB;
-}
-
-export const auth = betterAuth({
-    database: drizzleAdapter(getDB(), {
+    return betterAuth({
+        database: drizzleAdapter(_db, {
         provider: "sqlite",
         schema: schema
     }),
     emailAndPassword: {
         enabled: true
-    }
-})
+        }
+    })
+}
